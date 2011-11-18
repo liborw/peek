1e64c376e8489ede3c56b847af9b120aa0c579da
Libor Wagner <liborwagner@gmail.com>
2011-11-18 16:47:34 +0100

    Command line argument parsing and README listing
    Add option to specify directory or list all in current directory, then
    show firs three lines of README.md

--- psum.sh
+++ psum.sh
@@ -2,23 +2,43 @@
 
 usage() {
 cat<<EOF
-Usage: `basename $0` [-h]
+Usage: `basename $0` [options]
 
 This script displays summary of a project.
 
 Options:
 
-    -h  Show this message
+    -h --help  Show this message
 
 EOF
 }
 
-while getopts “hs” OPTION
-do
-    case $OPTION in
-        h)
-            usage
-            exit 1
-            ;;
-    esac
+while getopts ":h" opt
+do      case "$opt" in
+        h)      usage
+                exit 0;;
+        ?)      usage
+                exit 1;;
+        esac
 done
+
+shift $(($OPTIND -1))
+
+if [ -n ""$1"" ]
+then
+    DIRS="$@"
+else
+    DIRS=`ls -d */`
+fi
+
+
+bold=`tput bold`
+normal=`tput sgr0`
+
+for dir in $DIRS
+do
+    echo "${bold}${dir%/}:${normal}"
+
+    head -n 3 "${dir}README.md"
+done
+
